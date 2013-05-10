ffeda67a9a340fef56f57f1126705a4a50642b4b
Meris Williams <merisw@gmail.com>
2013-05-10 01:45:30 -0700

    Made links in decending order of recency

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkBlog::Application.config.secret_token = '0efcb4190db639e7a7ced63cd7bb32513ac3bdf71411b16e0bf7f9c2c633ed1bf2fca4413ef0d4b778abdd1404e3af56f53bf31d7a64cf83bb6de2cd70b74486'
